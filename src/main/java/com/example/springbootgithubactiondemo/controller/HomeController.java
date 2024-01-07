1193c96beedf7dc6a6dca400c822ca770c7300fd
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 16:58:53 -0500

    Updated code

--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -5,46 +5,25 @@ package com.example.springbootgithubactiondemo.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// @RestController
-// @RequestMapping("/")
-// public class HomeController {
-
-//     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-//     @GetMapping("app")
-//     public String index(){
-
-//         logger.info("Hello World! Testing Github Action");
-//         logger.debug("This is a debug message");
-//         logger.info("This is an info message");
-//         logger.warn("This is a warn message");
-//         logger.error("This is an error message");
-//         System.out.println("hello");
-//         return "Hello World! Testing Github Action";
-//     }
-// }
-
-import com.example.springbootgithubactiondemo.helpers.SpringLoggingHelper;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
-@RequestMapping("/app/")
+@RequestMapping("/")
 public class HomeController {
-    private static final Logger logger = LogManager.getLogger(HomeController.class);
-   
-    @GetMapping("hello")
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @GetMapping("app")
     public String index(){
+
+        logger.info("Hello World! Testing Github Action");
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warn message");
         logger.error("This is an error message");
-        new SpringLoggingHelper().helpMethod();
-        return "index";
+        System.out.println("hello");
+        return "Hello World! Testing Github Action";
     }
 }
--- src/main/java/com/example/springbootgithubactiondemo/helpers/SpringLoggingHelper.java
+++ src/main/java/com/example/springbootgithubactiondemo/helpers/SpringLoggingHelper.java
@@ -1,10 +1,12 @@
 package com.example.springbootgithubactiondemo.helpers;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SpringLoggingHelper {
-    private static final Logger logger = LogManager.getLogger(SpringLoggingHelper.class);
+    private static final Logger logger = LoggerFactory.getLogger(SpringLoggingHelper.class);
     public void helpMethod(){
         logger.debug("This is a debug message");
         logger.info("This is an info message");
