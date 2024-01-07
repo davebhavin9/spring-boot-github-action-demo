e30d60a99b7645d28c7b7e4aa2b5db6f492d3145
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 16:23:13 -0500

    Updated code

--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.example.springbootgithubactiondemo.controller;
 
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +32,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
+@RequestMapping("/app")
 public class HomeController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    @RequestMapping("/app")
+   
+    @GetMapping("hello")
     String index(){
         logger.debug("This is a debug message");
         logger.info("This is an info message");
