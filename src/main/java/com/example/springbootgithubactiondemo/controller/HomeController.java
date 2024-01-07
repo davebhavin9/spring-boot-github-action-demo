31f0747d6aefc8c3bfe77db841d08b9121417418
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 13:14:12 -0500

    Hello

--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.example.springbootgithubactiondemo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/")
 public class HomeController {
 
+    private static Logger logger = LogManager.getLogger();
+
     @GetMapping("home")
     public String index(){
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
         return "Hello World! Testing Github Action";
     }
 }
