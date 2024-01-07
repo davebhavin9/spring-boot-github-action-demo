6e510ed065ef52f45b31031bae1f9754c3a71b94
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 16:16:45 -0500

    Updated code

--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -1,27 +1,46 @@
 package com.example.springbootgithubactiondemo.controller;
 
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+
+// @RestController
+// @RequestMapping("/")
+// public class HomeController {
+
+//     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+//     @GetMapping("app")
+//     public String index(){
+
+//         logger.info("Hello World! Testing Github Action");
+//         logger.debug("This is a debug message");
+//         logger.info("This is an info message");
+//         logger.warn("This is a warn message");
+//         logger.error("This is an error message");
+//         System.out.println("hello");
+//         return "Hello World! Testing Github Action";
+//     }
+// }
+
+import com.example.springbootgithubactiondemo.helpers.SpringLoggingHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
-
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-    @GetMapping("app")
-    public String index(){
-
-        logger.info("Hello World! Testing Github Action");
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @RequestMapping("/")
+    String index(){
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warn message");
         logger.error("This is an error message");
-        System.out.println("hello");
-        return "Hello World! Testing Github Action";
+        new SpringLoggingHelper().helpMethod();
+        return "index";
     }
 }
