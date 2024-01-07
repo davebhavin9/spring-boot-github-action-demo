69a8efc90fc707d131b62da939fee0e1b0ba130a
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 16:50:33 -0500

    Updated code

--- src/main/java/com/example/springbootgithubactiondemo/helpers/SpringLoggingHelper.java
+++ src/main/java/com/example/springbootgithubactiondemo/helpers/SpringLoggingHelper.java
@@ -1,10 +1,10 @@
 package com.example.springbootgithubactiondemo.helpers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SpringLoggingHelper {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LogManager.getLogger(SpringLoggingHelper.class);
     public void helpMethod(){
         logger.debug("This is a debug message");
         logger.info("This is an info message");
