66e5c6faa4be1ea6bad6ca3f087141b29801bd63
Bhavin Dave <bhavin.dave@ticketmaster.com>
2024-01-07 16:27:07 -0500

    Updated code

--- src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
+++ src/main/java/com/example/springbootgithubactiondemo/controller/HomeController.java
@@ -32,7 +32,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/app/")
 public class HomeController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
    
